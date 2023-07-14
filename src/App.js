403ad01fbf7dcb26f11c0154ee341cf5e34af52a
Ronaldo <ronaldoandrade966@gmail.com>
2023-07-14 15:33:35 -0300

    filter by status and dates ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { empresas } from './empresas';
 
 function App() {
+  const [status, setStatus] = useState("");
+  const [initialDate, setInitialDate] = useState("");
+  const [finalDate, setFinalDate] = useState("");
+
+  const modelInitialDate = initialDate + " 00:00:00 GMT-3";
+  const modelFinalDate = finalDate + " 00:00:00 GMT-3";
+  const initialDateCalc = Date.parse(modelInitialDate);
+  const finalDateCalc = Date.parse(modelFinalDate);
+
+  const filteredCompanies = empresas.filter(empresa => {
+
+    if (initialDate || finalDate) {
+      const modelCompanyDate = empresa.dataInclusao.split("/").reverse().join("-") + " 00:00:00 GMT-3";
+      var companyDate = Date.parse(modelCompanyDate);
+    }
+
+    if (status && initialDate && finalDate) {
+      return (empresa.statusEmpresa === status && companyDate >= initialDateCalc && companyDate <= finalDateCalc)
+    }
+
+    if (initialDate && finalDate) {
+      return (companyDate >= initialDateCalc && companyDate <= finalDateCalc)
+    }
+
+    if (status && initialDate) {
+      return (empresa.statusEmpresa === status && companyDate >= initialDateCalc)
+    }
+
+    if (status && finalDate) {
+      return (empresa.statusEmpresa === status && companyDate <= finalDateCalc)
+    }
+
+    if (initialDate) {
+      return (companyDate >= initialDateCalc)
+    }
+
+    if (finalDate) {
+      return (companyDate <= finalDateCalc)
+    }
+
+    if (status) {
+      return empresa.statusEmpresa === status
+    }
+
+    return true;
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for='status'>Status da empresa</label>
+      <select id='status' onChange={(e) => setStatus(e.target.value)}>
+        <option value="">Todas</option>
+        <option value="ativa">Ativas</option>
+        <option value="inativa">Inativas</option>
+      </select>
+
+      <label for='initialDate'>Data inicial</label>
+      <input type='date' id='initialDate' value={initialDate} onChange={e => {setInitialDate(e.target.value)}}></input>
+      <label for='finalDate'>Data final</label>
+      <input type='date' id='finalDate' value={finalDate} onChange={e => {setFinalDate(e.target.value)}}></input>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Lista de empresas</th>
+          </tr>
+          <tr>
+            <th scope='col'>CNPJ</th>
+            <th scope='col'>Razão social</th>
+            <th scope='col'>Responsável</th>
+            <th scope='col'>Telefone</th>
+            <th scope='col'>E-mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredCompanies.map(empresa => <TableData empresa={empresa} key={empresa.codigo}/>)}
+        </tbody>
+      </table>
     </div>
   );
+}
+
+function TableData({empresa}) {
+  let statusEmpresa = "ativa";
+  if (empresa.statusEmpresa === "inativa") {
+    statusEmpresa = "inativa";
+  }
+
+  return (
+    <tr className={statusEmpresa}>
+      <td>{empresa.cnpj}</td>
+      <td>{empresa.razaoSocial}</td>
+      <td>{empresa.responsavelLegal}</td>
+      <td>{empresa.telefoneContato}</td>
+      <td>{empresa.email}</td>
+    </tr>
+  )
 }
 
 export default App;
---
+++ src/empresas.js
@@ -0,0 +1,24 @@
+export const empresas = [
+    {   
+        codigo: "001", 
+        cnpj: "89.623.011/0001-31", 
+        razaoSocial: "Ayla e Rafaela Limpeza Ltda",
+        inscricaoMunicipal: "20.553.987-4",
+        dataInclusao: "26/11/2003", 
+        statusEmpresa: "inativa", 
+        responsavelLegal: "Rafaela Andrade", 
+        email: "cobranca@aylaerafaelalimpezaltda.com.br", 
+        telefoneContato: "(84)2750-7686"
+    },
+    {   
+        codigo: "002", 
+        cnpj: "51.207.793/0001-55", 
+        razaoSocial: "Paulo e Laura Comercio de Bebidas ME",
+        inscricaoMunicipal: "20.010.213-3",
+        dataInclusao: "20/04/2020", 
+        statusEmpresa: "ativa", 
+        responsavelLegal: "Paulo Moreira", 
+        email: "presidencia@pauloelauracomerciodebebidasme.com.br", 
+        telefoneContato: "(84)2831-3548"
+    }
+]
